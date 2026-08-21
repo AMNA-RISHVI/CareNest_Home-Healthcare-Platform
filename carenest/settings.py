0d8d2c222951d8fb8f225f034fead012280a588f
hithara-core <manothmie17@gmail.com>
2026-08-21 23:10:51 +0530

    modify carenest

--- carenest/settings.py
+++ carenest/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adminpanel',
+    'professionalpanel',
 ]
+
+AUTH_USER_MODEL = 'adminpanel.User' 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom Login Redirect URL
+LOGIN_URL = '/accounts/login/'
